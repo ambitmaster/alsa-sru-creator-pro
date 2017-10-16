cdfa3555c31fb4a9a7d95cc08e0551d4a9cf0ad5
ambitmaster <alexander.r.mogren@gmail.com>
2017-10-16 02:31:42 +0200

    Update SruView.java

--- SRU/src/sru.alsa.views/SruView.java
+++ SRU/src/sru.alsa.views/SruView.java
@@ -2,11 +2,14 @@ package sru.alsa.views;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.AbstractButton;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import sru.alsa.controllers.SruController;
 
@@ -15,64 +18,104 @@ public class SruView extends javax.swing.JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	private SruController controller = new SruController();
 	private JFrame sruFrame;
-	private JButton openFile,createSruInfo,createSruBlank;
-	private JPanel panel;
+	private JTextField textField;
+	private JTextField textField_1;
 	
 	public SruView(){
 		initializeComponents();
 	}
 	
 	private void initializeComponents(){
-		
+
 		sruFrame = new JFrame("ALSA SRU PRO 1.0");
-		panel = new JPanel();
-		
+		sruFrame.setBounds(100, 100, 450, 300);
 		sruFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		sruFrame.setSize(400,250);
-		sruFrame.setLocationRelativeTo(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		sruFrame.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Läs från fil");
+		mnNewMenu.add(mntmNewMenuItem);
+		mntmNewMenuItem.addActionListener(this);
+		
+		JMenuItem mntmSkapaFil = new JMenuItem("Skapa info fil");
+		mnNewMenu.add(mntmSkapaFil);
+		mntmSkapaFil.addActionListener(this);
+
+		JMenuItem mntmSkapaFilBlnk = new JMenuItem("Skapa blankett fil");
+		mnNewMenu.add(mntmSkapaFilBlnk);
+		mntmSkapaFilBlnk.addActionListener(this);
+		
+		JMenuItem mntmAvsluta = new JMenuItem("Avsluta");
+		mnNewMenu.add(mntmAvsluta);
+		mntmAvsluta.addActionListener(this);
+		
+		sruFrame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 30, 139, 20);
+		sruFrame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Organistationsnummer");
+		lblNewLabel.setBounds(10, 11, 139, 14);
+		sruFrame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblRkenskapsr = new JLabel("Räkenskapsår");
+		lblRkenskapsr.setBounds(159, 11, 96, 14);
+		sruFrame.getContentPane().add(lblRkenskapsr);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(159, 30, 86, 20);
+		sruFrame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		textField_1.addActionListener(this);
+	
+		textField_1.getDocument().addDocumentListener(new DocumentListener() {
+			
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				controller.checkTextField(textField_1);
+			}
+			
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				controller.checkTextField(textField_1);
+			}
+			
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				controller.checkTextField(textField_1);
+			}
+
+		});
+		
 		sruFrame.setVisible(true);
 		
-		openFile = new JButton("open SRU file");
-		openFile.setVerticalTextPosition(AbstractButton.BOTTOM);
-		openFile.setHorizontalTextPosition(AbstractButton.CENTER);
 
-		openFile.addActionListener(this);
-		openFile.setVisible(true);
-
-		createSruInfo = new JButton("create INFO.SRU file");
-		createSruInfo.setVerticalTextPosition(AbstractButton.BOTTOM);
-		createSruInfo.setHorizontalTextPosition(AbstractButton.CENTER);
-
-		createSruInfo.addActionListener(this);
-		createSruInfo.setVisible(true);
-		
-		createSruBlank = new JButton("create BLANKETT.SRU file");
-		createSruBlank.setVerticalTextPosition(AbstractButton.BOTTOM);
-		createSruBlank.setHorizontalTextPosition(AbstractButton.CENTER);
-
-		createSruBlank.addActionListener(this);
-		createSruBlank.setVisible(true);
-
-		panel.setSize(400, 250);
-		panel.add(openFile);
-		panel.add(createSruInfo);
-		panel.add(createSruBlank);
-		sruFrame.getContentPane().add(panel);
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getActionCommand()=="open SRU file"){
+		if(e.getActionCommand()=="Läs från fil"){
 			controller.openFile();
 		}
 
-		if(e.getActionCommand()=="create INFO.SRU file"){
+		if(e.getActionCommand()=="Skapa info fil"){
 			controller.createSRUInfo();
 		}
 		
-		if(e.getActionCommand()=="create BLANKETT.SRU file"){
+		if(e.getActionCommand()=="Skapa blankett fil"){
 			controller.createSRUBlank();
 		}
+		
+		if(e.getActionCommand()=="Avsluta"){
+			controller.exitSru();
+		}
+
 	}
 }
